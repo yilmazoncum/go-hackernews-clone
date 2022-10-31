43d4213d297abce5687b9d7d0080d5f4ee204797
yilmazoncum <oncumkorkmaz@gmail.com>
2022-10-31 13:29:49 +0300

    add improved cache

--- server.go
+++ server.go
@@ -29,11 +29,30 @@ func main() {
 }
 
 func handler(numStories int, templ *template.Template) http.HandlerFunc {
+	sc := storyCache{
+		numStories: numStories,
+		duration:   6 * time.Second,
+	}
+
+	go func() {
+		ticker := time.NewTicker(3 * time.Second)
+		for {
+			temp := storyCache{
+				numStories: numStories,
+				duration:   6 * time.Second,
+			}
+			temp.stories()
+			sc.mutex.Lock()
+			sc.cache = temp.cache
+			sc.expiration = temp.expiration
+			sc.mutex.Unlock()
+			<-ticker.C
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-
-		stories, err := getCachedStories(numStories)
+		stories, err := sc.stories()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -52,25 +71,27 @@ func handler(numStories int, templ *template.Template) http.HandlerFunc {
 	})
 }
 
-var cache []item
-var cacheExpiration time.Time
-var cacheMutex sync.Mutex
+type storyCache struct {
+	numStories int
+	cache      []item
+	expiration time.Time
+	duration   time.Duration
+	mutex      sync.Mutex
+}
 
-func getCachedStories(numStories int) ([]item, error) {
-	cacheMutex.Lock()
-	defer cacheMutex.Unlock()
-	if time.Now().Sub(cacheExpiration) < 0 {
-		fmt.Print("Cached Storyies Return !!!")
-		return cache, nil
+func (sc *storyCache) stories() ([]item, error) {
+	sc.mutex.Lock()
+	defer sc.mutex.Unlock()
+	if time.Now().Sub(sc.expiration) < 0 {
+		return sc.cache, nil
 	}
-	fmt.Print("getTopStories() ????")
-	stories, err := getTopStories(numStories)
+	stories, err := getTopStories(sc.numStories)
 	if err != nil {
 		return nil, err
 	}
-	cache = stories
-	cacheExpiration = time.Now().Add(5 * time.Minute)
-	return cache, nil
+	sc.cache = stories
+	sc.expiration = time.Now().Add(sc.duration)
+	return sc.cache, nil
 }
 
 func getTopStories(numStories int) ([]item, error) {
